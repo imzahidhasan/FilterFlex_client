9994cfe664cb748d6a2aec46662a4e53420cd15b
Zahid Hasan <imzahidhasan.mail@gmail.com>
2024-08-15 23:39:47 +0600

    added sort option box

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -71,7 +71,11 @@ function ProductList() {
         setSearchTerm(searchText);
         setCurrentPage(1);
     };
+    const handleSort = (e) => {
+        e.preventDefault()
+        const sort = e.target.value
 
+    }
     return (
         <div>
             {/* search box */}
@@ -88,6 +92,22 @@ function ProductList() {
                         <button type="submit" className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
                     </div>
                 </form>
+            </div>
+            {/* sort box section */}
+            <div>
+                <form onChange={handleSort} className="max-w-40">
+                    <label htmlFor="countries" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select an option</label>
+                    <select id="countries" name='sort' className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                        <option disabled selected>Sort by</option>
+                        <option value="1">Newest first</option>
+                        <option value="2">Low to high</option>
+                        <option value="3">High to low</option>
+                    </select>
+                </form>
+
+
+
+
             </div>
             {/* product card section */}
             <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
